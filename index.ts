0f9209a19ddf1d3ee287178d95160656c307c173
salah595 <s.alhashmi.13456@gmail.com>
2022-06-30 12:46:12 +0400

    3 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,39 @@
-// Import stylesheets
 import './style.css';
 
-// Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const prevButton : HTMLButtonElement = document.getElementById("prev");
+const nextButton : HTMLButtonElement= document.getElementById("next");
+const levelNumber : HTMLParagraphElement = document.getElementById("number");
+const 
+// initialize game
+// fetch the level and the scores from local storage
+let level = 1;
+levelNumber.innerText = '' + level;
+const colorArr:string[] = ['red', 'blue', 'black'];
+const steps = []; 
+
+function createButtonEl (color: string, id: string) : HTMLLabelElement {
+  const button = document.createElement("label");
+  button.classList.add("game-btn");
+  button.style.backgroundColor = color;
+  button.id = id;
+
+  return button;
+}
+
+
+
+function gameLoop() {
+
+}
+// each level has a button
+// append to the dom
+// create a step
+// disable button
+// create the animation
+// test user and check
+// if true move 
+// if false retry
+
+
